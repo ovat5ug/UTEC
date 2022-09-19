81a1291dfac37fbe2429367ef73d975d1f3a6c12
Gustavo Pérez <yahoo_3257@hotmail.com>
2022-09-18 21:40:17 -0600

    Update album.dart
    creacion de statefulwidget para album, en este espacio se comnzara a dibujar la carga de albumes

--- ELECTIVA 03/PERIODO 02/clone_spotify/lib/paginas/album.dart
+++ ELECTIVA 03/PERIODO 02/clone_spotify/lib/paginas/album.dart
@@ -1,1 +1,25 @@
-//captura evento de toque y carga contenido al precionar algun "album">
+//captura evento de toque y carga contenido al precionar algun "album"
+import 'package:flutter/material.dart';
+import 'package:clone_spotify/temas/colores.dart';
+import 'package:flutter_feather_icons/flutter_feather_icons.dart';
+import 'package:clone_spotify/paginas/contenedores.dart';
+import 'package:clone_spotify/paginas/principal.dart';
+import 'package:clone_spotify/json/canciones_json.dart';
+
+class AlbumesPage extends StatefulWidget {
+  const AlbumesPage({super.key});
+
+  @override
+  State<AlbumesPage> createState() => _AlbumesPageState();
+}
+
+class _AlbumesPageState extends State<AlbumesPage> {
+  final dynamic canciones = null;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        //este apartado es el que se encarga de retornar la hoja del album
+
+        );
+  }
+}
