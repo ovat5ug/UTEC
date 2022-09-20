017ff2e1db57dd295a097890eac8c8da3afee114
Gustavo Pérez <yahoo_3257@hotmail.com>
2022-09-20 15:20:23 -0600

    añadiendo variables
    se añadieron nuevos colores para poder testear y 2 variables globales

--- ELECTIVA 03/PERIODO 02/clone_spotify/lib/temas/colores.dart
+++ ELECTIVA 03/PERIODO 02/clone_spotify/lib/temas/colores.dart
@@ -4,8 +4,16 @@ const Color primario = Color(0xE835A509);
 const Color blanco = Color.fromARGB(204, 255, 255, 255);
 const Color gris = Color.fromRGBO(134, 132, 134, 0.608);
 const Color negro = Color(0x0F174C8A);
+const Color azul = Color.fromARGB(255, 0, 60, 170);
+const Color amarillo = Color.fromARGB(255, 201, 181, 5);
+const Color rojo = Color.fromARGB(255, 156, 22, 12);
+const Color rosado = Color.fromARGB(255, 221, 14, 159);
+const Color musgo = Color.fromARGB(255, 11, 78, 16);
+const Color mostaza = Color.fromARGB(255, 105, 104, 4);
 
 /*INICIO variables de contenedores*/
 int menu_activo = 0;
 int menu_activo2 = 0;
+int menu_activo3 = 0;
+int menu_activo4 = 0;
 /*FIN variables de contenedores*/
