5214479751c3daad862e63bd0c9cb68365913d64
Gustavo Pérez <yahoo_3257@hotmail.com>
2022-09-20 23:30:41 -0600

    tira de albumes
    se añadió una tira de álbumes que se ve dentro de la pantalla de álbumes detalle

--- ELECTIVA 03/PERIODO 02/clone_spotify/lib/paginas/album.dart
+++ ELECTIVA 03/PERIODO 02/clone_spotify/lib/paginas/album.dart
@@ -105,6 +105,85 @@ class _AlbumesPageState extends State<AlbumesPage> {
               SizedBox(
                 height: 30,
               ),
+//INICIO tira de albumes
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 30),
+                  child: Row(
+                    children: List.generate(songs.length, (index) {
+                      return Padding(
+                        padding: const EdgeInsets.only(right: 30),
+                        child: GestureDetector(
+                          onTap: () {},
+                          child: Column(
+                            children: [
+                              Container(
+                                width: 180,
+                                height: 180,
+                                decoration: BoxDecoration(
+                                    image: DecorationImage(
+                                        image: AssetImage(songs[index]['img']),
+                                        fit: BoxFit.cover),
+                                    color: primario,
+                                    borderRadius: BorderRadius.circular(10)),
+                              ),
+                              SizedBox(
+                                height: 20,
+                              ),
+                              Text(
+                                songs[index]['title'], //titulo del album
+                                style: TextStyle(
+                                    fontSize: 15,
+                                    color: blanco,
+                                    fontWeight: FontWeight.w600),
+                              ),
+                              SizedBox(
+                                height: 5,
+                              ),
+                              Container(
+                                //width: tamanho.width - 210,
+                                width: 160,
+                                height: 50,
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Text(
+                                      songs[index][
+                                          'song_count'], //muestra conteo de canciones
+                                      maxLines: 1,
+                                      textAlign: TextAlign.center,
+                                      style: TextStyle(
+                                          fontSize: 12,
+                                          color: gris,
+                                          fontWeight: FontWeight.w600),
+                                    ),
+                                    Text(
+                                      songs[index]
+                                          ['date'], //duracion total del album
+                                      maxLines: 1,
+                                      textAlign: TextAlign.center,
+                                      style: TextStyle(
+                                          fontSize: 12,
+                                          color: gris,
+                                          fontWeight: FontWeight.w600),
+                                    )
+                                  ],
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      );
+                    }),
+                  ),
+                ),
+              ),
+//FIN tira de albumes
+              SizedBox(
+                height: 30,
+              ),
               Padding(
                 padding: const EdgeInsets.only(
                   left: 30,
