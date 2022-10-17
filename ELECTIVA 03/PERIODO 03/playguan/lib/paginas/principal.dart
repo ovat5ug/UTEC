9bbbb9f7f456b6ce1990debfa0b88b9917734754
Gustavo Pérez <yahoo_3257@hotmail.com>
2022-10-17 09:51:47 -0600

    Create principal.dart
    creando carpeta de paginas y el espacio de principal

---
+++ ELECTIVA 03/PERIODO 03/playguan/lib/paginas/principal.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class paginaPrincipal extends StatelessWidget {
+  const paginaPrincipal({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
