9512cbc520488687f51a2e7a5375d89fd8f807f0
Gustavo Pérez <yahoo_3257@hotmail.com>
2022-10-18 09:40:31 -0600

    Create variablesGoblales.dart

---
+++ ELECTIVA 03/PERIODO 03/playguan/lib/paginas/variablesGoblales.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+//COLORES DE PLAYGUAN
+const azulPrincipal = Color.fromARGB(255, 1, 141, 177); //HEX #018db1
+const blanco = Color.fromARGB(255, 255, 255, 255);
+const grisTransparente = Color.fromARGB(162, 41, 43, 55);
+const gris = Color.fromARGB(255, 41, 43, 55);
+const negro = Color.fromARGB(255, 0, 0, 0);
+const azulSecundario = Color.fromARGB(255, 83, 135, 240);//HEX #5388f0
