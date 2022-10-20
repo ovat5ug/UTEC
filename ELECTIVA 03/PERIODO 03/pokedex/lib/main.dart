9616caa3cace71c9e84b6b59f6bd74e3bf8f94c1
Gustavo Pérez <yahoo_3257@hotmail.com>
2022-10-19 21:59:16 -0600

    Update main.dart
    captura de informacion de la API

--- ELECTIVA 03/PERIODO 03/pokedex/lib/main.dart
+++ ELECTIVA 03/PERIODO 03/pokedex/lib/main.dart
@@ -1,17 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:pokedex/paginas/gif.dart';
 import 'package:pokedex/paginas/home.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert' as convert;
 
 void main() {
-  runApp(const pokeDesk());
+  runApp(pokeDesk());
 }
 
 class pokeDesk extends StatelessWidget {
-  const pokeDesk({super.key});
+  //const pokeDesk({super.key});
+  late Future<List<Gif>> _listadegif;
+
+  Future<List<Gif>> _getGifs() async {
+    final response = await http.get(Uri.parse(
+        "https://api.giphy.com/v1/gifs/trending?api_key=sw1fM8if1R79iCYYzGzG4QvF4ix8Mp7O&limit=10&rating=g"));
+    if (response.statusCode == 200) {
+      print(response.body);
+    } else {
+      throw Exception("Falla al gerenar el JSON");
+    }
+    return _listadegif;
+  }
+
+  @override
+  void initState() {
+    _getGifs();
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: principalHome(),
+      title: 'Api Rest',
+      //home: Scaffold(
+      //  appBar: AppBar(
+      //  title: Text('Material Appi'),
+      //),
+      //body: Center(
+      //child: Container(child: Text("saludos")),
+      //)),
     );
   }
 }
